6bac9e502143d2cf5f6547fdb2e1ebce581fbbf3
heyitsuzair <uzairdevil354123@gmail.com>
2022-10-01 04:03:27 +0500

    Peek A Boo :o

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useTranslation } from "react-i18next";
+import i18next from "i18next";
+import cookies from "js-cookie";
+import { useEffect } from "react";
+
+const languages = [
+  {
+    code: "fr",
+    name: "Français",
+    country_code: "fr",
+    dir: "rtl",
+  },
+  {
+    code: "en",
+    name: "English",
+    country_code: "gb",
+  },
+];
 
 function App() {
+  const currentLangCode = cookies.get("i18next") || "en";
+  const currentLang = languages.find((l) => l.code === currentLangCode);
+  const { t } = useTranslation();
+  const number_of_days = "uzair";
+
+  useEffect(() => {
+    document.body.dir = currentLang.dir || "ltr";
+    document.title = t("app_title");
+  }, [currentLang, t]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="d-flex flex-column align-items-center">
+        {t("days_since_release", { number_of_days })}
+      </div>
+      <div className="row">
+        {languages.map((lang) => {
+          return (
+            <button
+              disabled={lang.code === currentLangCode}
+              onClick={() => i18next.changeLanguage(lang.code)}
+            >
+              <div key={lang.country_code}>{lang.name}</div>
+              <span
+                style={{ opacity: lang.code === currentLangCode ? ".5" : "1" }}
+                className={`fi fi-${lang.country_code}`}
+              ></span>
+            </button>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from "i18next-http-backend";
+import App from "./App";
+import "/node_modules/flag-icons/css/flag-icons.min.css";
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    supportedLngs: ["en", "fr"],
+    fallbackLng: "en",
+    detection: {
+      order: [
+        "path",
+        "cookie",
+        "htmlTag",
+        "querystring",
+        "localStorage",
+        "sessionStorage",
+        "navigator",
+        "subdomain",
+      ],
+      caches: ["cookie"],
+    },
+    backend: {
+      loadPath: "/assets/locales/{{lng}}/translation.json",
+    },
+  });
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const loadingMarkup = <div className="text">Loading...</div>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Suspense fallback={loadingMarkup}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Suspense>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
